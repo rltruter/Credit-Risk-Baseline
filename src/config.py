05fcb5f5547a67c412ef2e67b8af5bca63a59699
rltruter <63010841+rltruter@users.noreply.github.com>
2026-01-13 13:16:38 -0500

    Create config.py
    Contains Bad Loan criteria and Data Leakage Columns

---
+++ src/config.py
@@ -0,0 +1,16 @@
+BAD_STATUSES = [
+    "Charged Off",
+    "Default",
+    "Late (31-120 days)",
+    "Late (16-30 days)"
+]
+
+LEAKAGE_COLS = [
+    "last_pymnt_d",
+    "last_pymnt_amnt",
+    "total_pymnt",
+    "recoveries",
+    "collection_recovery_fee"
+]
+
+DTI_CAP = 60
